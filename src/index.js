c5b4ff96cb22738fc2d718fd424c0495fc163635
jenwit-dev <johnny.js19@gmail.com>
2025-05-18 00:56:00 +0700

    css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <div className="header">
+        <h1>Movie App</h1>
+      </div>
+      {/* Input */}
+      <div className="search">
+        <input type="text" placeholder="Keyword ?" />
+        <button>search</button>
+      </div>
+      {/* Result */}
+      <div className="movie-lists">
+        <div className="movie">Movie 1</div>
+        <div className="movie">Movie 2</div>
+      </div>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
